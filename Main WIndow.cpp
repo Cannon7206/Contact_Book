511bc374544afd9536af7b6f4306790bc55fd790
christian <christianphilpot06@gmail.com>
2025-06-27 15:01:36 -0400

    Beginning to add an ui to the application

---
+++ Main WIndow.cpp
@@ -0,0 +1,5 @@
+//
+// Created by christian on 6/27/25.
+//
+
+#include "Main WIndow.h"
---
+++ Main WIndow.h
@@ -0,0 +1,8 @@
+//
+// Created by christian on 6/27/25.
+//
+
+#ifndef MAIN_WINDOW_H
+#define MAIN_WINDOW_H
+
+#endif //MAIN_WINDOW_H
