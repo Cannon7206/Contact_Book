ad6cd23d162de6a40b6cc15e4e2a99cf69dacacc
christian <christianphilpot06@gmail.com>
2025-07-01 08:44:02 -0400

    Fixed a bug from an old class

--- main.cpp
+++ main.cpp
@@ -3,10 +3,11 @@
 
 int main(int argc, char *argv[])
 {
+    //start application
     QApplication app(argc, argv);
-
+    //create window
     MainWindow window;
+    //show window
     window.show();
-
     return app.exec();
 }
